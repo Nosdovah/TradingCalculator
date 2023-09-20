bfbc11055d732c0601cfffccc5be037afa45761f
Nosdovah <nosdovah@LAPTOP-239E2HQR>
2023-09-20 16:32:14 +0700

    revert back to the beginning

---
+++ script.js
@@ -0,0 +1,12 @@
+var result = document.getElementById("resDisplay")
+var count = document.getElementById("countDisplay")
+var equalBtn = document.getElementsByClassName('equal')
+
+//ko ga work sih AJG kesel gua
+equalBtn.onclick = function () {
+    count.value = "test"
+}
+
+
+
+
