a472a1ba0a25e2e768bad5557d3d561bba63b936
Nosdovah <nosdovah@LAPTOP-239E2HQR>
2023-09-20 18:20:37 +0700

    delete all onclick on html

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-var result = document.getElementById("resDisplay")
-var count = document.getElementById("countDisplay")
-var equalBtn = document.getElementsByClassName('equal')
+var countDisplay = document.getElementById('countDisplay')
+var resultDisplay = document.getElementById('resDisplay')
 
-//ko ga work sih AJG kesel gua
-equalBtn.onclick = function () {
-    count.value = "test"
-}
+// inp.forEach(element => {
+//     //addEventListener ga bisa untuk input type=button ?
+//     element.addEventListener('onclick', function () {
+//         console.log(`${element.value} is clicked`)
+//     })
+//     //onclick function untuk input type button
+//     element.onclick = function () {
+        
+//     }
+// })
+
+//var equalBtn = document.getElementsByClassName('equal')
+
 
 
 
