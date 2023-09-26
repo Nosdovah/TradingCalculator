e2a6c6e21115658ed84b5f867b46148f5f3a3514
Nosdovah <raihanhaniffirdaus@gmail.com>
2023-09-26 12:46:09 +0700

    display on html , working on the logic

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
 var countDisplay = document.getElementById('countDisplay')
 var resultDisplay = document.getElementById('resDisplay')
+const buttonClick = document.querySelectorAll(`input[type="button"]`)
+
+let save = 0;
+let res = 0;
+
+//Gotta make the calculate logic
+
+    buttonClick.forEach(element => {
+        element.addEventListener('click',function() {
+            if(this.classList.value == "operator") {
+                //add on text value
+                countDisplay.value += this.value
+            }else {
+                    if(this.value === "AC") {
+                        countDisplay.value = ''
+                    }
+                    else if(this.value === "C") {
+                        countDisplay.value = countDisplay.value.toString().slice(0,-1)
+                        // if(!last.length == 0) {
+                        //     last = last.slice(0,-1)
+                        // }
+                    }else {
+                        countDisplay.value += this.value
+                    }
+                }
+            
+            
+        })
+    })
+
+
+
 
 // inp.forEach(element => {
 //     //addEventListener ga bisa untuk input type=button ?
