d0391907c9fb5ce22929c5b848886acb7f44f4ef
Nosdovah <raihanhaniffirdaus@gmail.com>
2023-10-03 08:01:33 +0700

    alpha 0.1

--- script.js
+++ script.js
@@ -8,28 +8,34 @@ class Calculator {
 
     clear() {
         this.preOperand = ''
-        this.postOperand = ''
+        this.postOperand = '0'
         this.operator = undefined
     }
 
     delete() {
-
+        //current operand ?
+        this.postOperand = this.postOperand.toString().slice(0, -1)
+        if(this.postOperand.length == 0) {
+            this.postOperand = this.postOperand + "0"
+        }
     }
 
 
     addNumber(number) {
-        //If you click zero first thing first
-        //if (this.preOperand[0] == "0") this.preOperand.slice(0,-1 )
-
         //if you click multiple dot (.)
         if (number === "." && this.preOperand.includes(".")) return
         this.postOperand += number.toString()
+        //If you click zero first thing first
+        if (this.postOperand[0] === "0")  {
+           this.postOperand =  this.postOperand.replace("0","")
+        }
+
     }
 
 
     operation(operand) {
-        if(this.postOperand === '') return
-        if(this.preOperand !== '') {
+        if (this.postOperand === '') return
+        if (this.preOperand !== '') {
             this.compute()
         }
         this.operator = operand
@@ -41,14 +47,46 @@ class Calculator {
         this.postOperandText.textContent = this.postOperand
         this.preOperandText.textContent = this.preOperand
 
+        if (this.operator !== undefined) {
+            this.preOperandText.textContent = this.preOperand + " " + this.operator
+        }
+
     }
 
     compute() {
-        if(isNaN(prev) || isNaN(current) ) return
         const prev = parseFloat(this.preOperand)
+        // //Testing prev 
+        // console.log(`prev ${prev}`)
         const current = parseFloat(this.postOperand)
-          
+        //testing current
+        // console.log(`current ${current}`)
+
+        let computation
+        if (isNaN(prev) || isNaN(current)) return
+        if (this.operation == null || this.operation == undefined) return
+        // //test operator 
+        // console.log(`operator ? ${this.operator}`)
+        if (this.operator === "+") {
+            computation = prev + current
+        }
+        if (this.operator == "-") {
+            computation = prev - current
+        }
+        if (this.operator == "%") {
+            computation = prev % current
+        }
+        if (this.operator == "x") {
+            computation = prev * current
+        }
+        if (this.operator == ":") {
+            computation = prev / current
+        }
+        this.postOperand = computation
+        this.operator = undefined
+        this.preOperand = ''
     }
+
+
 }
 
 const numbers = document.querySelectorAll('[numbers]');
@@ -62,16 +100,10 @@ const postOperandText = document.querySelector('[post-number]')
 
 const calculator = new Calculator(preOperandText, postOperandText);
 
-// numbers.forEach(button => {
-//     button.addEventListener('click' , () => {
-//         calculator.addNumber(button.textContent)
-//         calculator.displayUpdate()
-//     })
-// })
+
 clear.addEventListener('click', () => {
     calculator.clear()
-    preOperandText.textContent = ""
-    postOperandText.textContent = "0"
+    calculator.displayUpdate()
 })
 
 
@@ -89,4 +121,12 @@ operand.forEach(button => {
     })
 })
 
+equal.addEventListener('click', () => {
+    calculator.compute()
+    calculator.displayUpdate()
+})
 
+del.addEventListener('click', () => {
+    calculator.delete()
+    calculator.displayUpdate()
+})
